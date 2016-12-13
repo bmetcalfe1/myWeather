9dc197d13cf1c23658b7eb8687a4561cd0642d14
Brendan Metcalfe <bgmetcalfe@gmail.com>
2016-12-12 19:51:46 -0500

    more refactor

--- my-weather.js
+++ my-weather.js
@@ -6,8 +6,11 @@ $(document).ready(function() {
     $.get("http://ipinfo.io", function(location) {
         console.log(location);
         var city = location.city;
+        var country = location.country;
+        // console.log(country);
         getPhoto(city);
-        return city;
+        getWeather(city + ", " + country);
+        // return city + ", " + country;
       }, 'jsonp');;
   }
 
@@ -15,76 +18,81 @@ $(document).ready(function() {
     var cityLink = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
     $.getJSON(cityLink,
       {
-          tags: city + " skyline",
+          tags: city,
           tagmode: "any",
           format: "json"
       },
       function(data) {
+        console.log(data);
         var rnd = Math.floor(Math.random() * data.items.length);
+        if (rnd === 0) {
+          console.log("No images to show!");
+        }
         var image_src = data.items[rnd]['media']['m'].replace("_m", "_b");
         $('html').css('background-image', "url('" + image_src + "')");
     });
   }
 
-  $("#location-choice").on('submit', function (e) {
-    var currentCity = e.currentTarget[0].value;
-    var currentCountry = e.currentTarget[1].value;
-    getPhoto(currentCity);
+  function addIcon(weather) {
+    // for loop to add hide to all div icons children
+    var div = document.getElementById('icons');
+    var divChildren = div.childNodes; // get an array of child nodes
+
+    for (var i=0; i<divChildren.length; i++) {
+        divChildren[i].className += " hide";
+    }
+    if ($('div.' + weather).hasClass('hide')) {
+      $('div.' + weather).removeClass('hide');
+    }
+  }
+
+  function iconGen(weather) {
+    var weather = weather.toLowerCase();
+    switch (weather) {
+      case 'drizzle':
+        addIcon(weather)
+        break;
+      case 'clouds':
+        addIcon(weather)
+        break;
+      case 'rain':
+        addIcon(weather)
+        break;
+      case 'snow':
+        addIcon(weather)
+        break;
+      case 'clear':
+        addIcon(weather)
+        break;
+      case 'mist':
+        addIcon(weather)
+        break;
+      case 'thunderstom':
+        addIcon(weather)
+        break;
+      default:
+        console.log("defaulting");
+        $('div.clouds').removeClass('hide');
+    }
+  }
+
+  function getWeather(location) {
+    console.log(location);
+    var city = location.split(',')[0];
+    var country = location.split(', ')[1];
+    console.log(city);
+    console.log(country);
 
     var weatherKey = "3d116075e0fe88576d7d105ffb94897e";
-    var weatherApiLink = "http://api.openweathermap.org/data/2.5/weather?q=" + currentCity + "&appid=" + weatherKey;
+    var weatherApiLink = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + weatherKey;
+
     $.getJSON(weatherApiLink, function(response) {
+      
       // need some error handling...
       console.log(response);
-      // ***
-      // weather-profile
-      // ***
-      function iconGen(weather) {
-        var weather = weather.toLowerCase();
-        switch (weather) {
-          case 'drizzle':
-            addIcon(weather)
-            break;
-          case 'clouds':
-            addIcon(weather)
-            break;
-          case 'rain':
-            addIcon(weather)
-            break;
-          case 'snow':
-            addIcon(weather)
-            break;
-          case 'clear':
-            addIcon(weather)
-            break;
-          case 'mist':
-            addIcon(weather)
-            break;
-          case 'thunderstom':
-            addIcon(weather)
-            break;
-          default:
-            console.log("defaulting");
-            $('div.clouds').removeClass('hide');
-        }
-      }
 
-      function addIcon(weather) {
-        // for loop to add hide to all div icons children
-        var div = document.getElementById('icons');
-        var divChildren = div.childNodes; // get an array of child nodes
-
-        for (var i=0; i<divChildren.length; i++) {
-            divChildren[i].className += " hide";
-        }
-        if ($('div.' + weather).hasClass('hide')) {
-          $('div.' + weather).removeClass('hide');
-          $('.detail-description').text(response.weather[0].description);
-        }
-      }
-      
       var theWeather = response.weather[0].main;
-      iconGen(theWeather); //calls function
+      iconGen(theWeather);
 
       // ***
       // weather-data
@@ -92,13 +100,15 @@ $(document).ready(function() {
       var tempCel = ((response.main.temp - 273.15).toFixed(2) + " °C");
       var tempFar = ((( response.main.temp - 273.15) * 9/5) + 32).toFixed(2) + " °F";
       //countries where farhenheit
-      if (currentCountry === "US" ||
-          currentCountry === "BZ" ||
-          currentCountry === "KY" ||
-          currentCountry === "GU" ||
-          currentCountry === "PR" ||
-          currentCountry === "PW" ||
-          currentCountry === "VA") 
+      $('.detail-description').text(response.weather[0].description);
+      $('.locale').text(location);
+      if (country === "US" ||
+          country === "BZ" ||
+          country === "KY" ||
+          country === "GU" ||
+          country === "PR" ||
+          country === "PW" ||
+          country === "VA") 
       {
         $('.temps').text(tempFar);  
       }
@@ -132,10 +142,16 @@ $(document).ready(function() {
 
       $('.sunrise').text(formatSunrise);
       $('.sunset').text(formatSunset);
-    });
+      });
+    }
 
-   //stop form submission
-   e.preventDefault();
+  $("#location-choice").on('submit', function (e) {
+    var currentCity = e.currentTarget[0].value;
+    var currentCountry = e.currentTarget[1].value;
+    getPhoto(currentCity);
+    getWeather(currentCity + ", " + currentCountry);
+    //stop form submission
+    e.preventDefault();
   });
 
 }); //doc ready
